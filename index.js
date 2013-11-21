a5febadacf098fb9e93178c6c3455af2afefe0a3
tom zhou <zs68j2ee@gmail.com>
2013-11-21 23:43:42 +0800

    fixed typo

--- index.js
+++ index.js
@@ -4,38 +4,41 @@ exports = module.exports = function(port) {
     var althttpp = /httpp:[0-9]+/gi;
     port = port || 80;
 
-	return function(req, res, next){
-	    var reshed = {};
-	    var _res_writeHead = res.writeHead;
-	    
-	    res.writeHead = function(statusCode, reasonPhrase, headers) {	        
-	        reshed.statusCode = statusCode;
-	        reshed.headers = {};
-	        
-	        if (typeof reasonPhrase === 'object') {
-	            reshed.headers = reasonPhrase;
-	        } else if (typeof headers === 'object') {
-	            reshed.headers = headers;
-	        }
-	        
-	        Object.keys(reshed.headers).forEach(function (key) {
-		        res.setHeader(key, reshed.headers[key]);
-		    });
-		    
-		    // set httpp capacity
-		    if (res.headers && ('alternate-protocol' in res.headers)) {
-		        if (!althttpp.test(res.headers['alternate-protocol'])) {
-		            res.headers['alternate-protocol'] = 'httpp:'+port+',' + 
-		                                                 res.headers['alternate-protocol'];
-		        }
-		    } else {
-		        res.setHeader('alternate-protocol', 'httpp:'+port);
-		    }
-		    
-		    // write statusCode
-		    _res_writeHead(reshed.statusCode || 200);
-	    };
-	    
-	    if (next) next();
-	};
+    return function(req, res, next){
+        var reshed = {};
+        var _res_writeHead = res.writeHead;
+
+        res.writeHead = function(statusCode, reasonPhrase, headers) {
+            reshed.statusCode = statusCode;
+            reshed.headers = {};
+
+            if (typeof reasonPhrase === 'object') {
+                reshed.headers = reasonPhrase;
+            } else if (typeof headers === 'object') {
+                reshed.headers = headers;
+            }
+
+            Object.keys(reshed.headers).forEach(function(key) {
+                res.setHeader(key, reshed.headers[key]);
+            });
+
+            // set httpp capacity
+            var altproto = res.getHeader('alternate-protocol');
+
+            if (altproto && altproto.length) {
+                if (!althttpp.test(altproto)) {
+                    altproto = 'httpp:'+port+',' + altproto;
+                }
+            } else {
+                altproto = 'httpp:'+port;
+            }
+            res.setHeader('alternate-protocol', altproto);
+
+            // write statusCode
+            res.writeHead = _res_writeHead;
+            res.writeHead(reshed.statusCode || 200);
+        };
+
+        if (next) next();
+    };
 };
